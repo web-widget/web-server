18adf18c2e5c0b3bec7da4e9d29dad704ffc15c6
糖饼 <sugarpie.tang@gmail.com>
2023-07-17 22:09:45 +0800

    chore: mod.ts -> index.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import type { Options } from "tsup";
 export const tsup: Options = {
   entry: {
-    server: "src/server/mod.ts",
-    client: "src/client/mod.ts",
+    server: "src/server/index.ts",
+    client: "src/client/index.ts",
   },
   dts: true,
   target: "es2022",
